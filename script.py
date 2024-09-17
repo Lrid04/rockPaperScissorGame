3a6b6902b727babfe31d90af7e00e17517433ce4
Lrid04 <landon.riddik04@gmail.com>
2024-09-17 10:29:06 -0500

    added route for game

--- script.py
+++ script.py
@@ -7,5 +7,9 @@ app.secret_key = "shh"
 def index():
     return render_template("index.html")
 
+@app.route("/game")
+def game():
+    return render_template("game.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
