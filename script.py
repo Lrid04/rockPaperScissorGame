40d9d7ca201283ef7d45cb7ced10cb7898b8d9fd
Swinglex <153555916+Swinglex@users.noreply.github.com>
2024-09-17 12:51:56 -0500

    plz fix

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ app.secret_key = "shh"
 def index():
     return render_template("index.html")
 
-@app.route("/game", methods=['POST'])
+@app.route("/game", methods=['GET', 'POST'])
 def game():
     my_game = BackEnd
 
