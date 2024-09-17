476d0eb72969310989899cd7a901e156da413267
Aiden <fecalmattertastesreallygood@gmail.com>
2024-09-17 10:19:26 -0500

    Added Backend

---
+++ BackEnd.py
@@ -0,0 +1,44 @@
+from random import randrange
+
+class BackEnd:
+    total_player_wins: int
+    total_computer_wins: int
+    options: list[1, 2, 3]
+    player_choice: str
+    computer_choice: str
+
+    def pick_option(self):
+        if self.options == 1:
+            self.player_choice = "Rock"
+        elif self.options == 2:
+            self.player_choice = "Paper"
+        else:
+            self.player_choice = "Scissors"
+
+    def computer_option(self):
+        computer_picked = randrange(1, 3)
+        if computer_picked == 1:
+            self.computer_choice = "Rock"
+        elif computer_picked == 2:
+            self.computer_choice = "Paper"
+        else:
+            self.computer_choice = "Scissors"
+
+    def rock_paper_scissors(player_choice, computer_choice, total_player_wins, total_computer_wins):
+        if player_choice == computer_choice:
+            return "Tie!"
+        elif (player_choice == "rock" and computer_choice == "scissors") or \
+                (player_choice == "scissors" and computer_choice == "paper") or \
+                (player_choice == "paper" and computer_choice == "rock"):
+            total_player_wins += 1
+            return "You win!"
+        else:
+            total_computer_wins += 1
+            return "Computer wins!"
+
+    def __init__(self, total_wins, total_losses, options, player_choice, computer_choice):
+        self.total_wins = total_wins
+        self.total_losses = total_losses
+        self.options = options
+        self.player_choice = player_choice
+        self.computer_choice = computer_choice
